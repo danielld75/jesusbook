8b32a9794313fa113440d6129e722a94fb06ed5d
danielld75 <danielld@poczta.fm>
2015-05-21 22:34:10 +0200

    add user_id to baptisms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521104917) do
+ActiveRecord::Schema.define(version: 20150521155813) do
 
   create_table "baptisms", force: :cascade do |t|
     t.date     "date"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20150521104917) do
     t.string   "minister"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "baptisms", ["user_id"], name: "index_baptisms_on_user_id"
 
   create_table "confirmations", force: :cascade do |t|
     t.date     "date"
