afb6e423d5a8ad1d65c8d80162c8a203b6df96f6
danielld75 <danielld@poczta.fm>
2015-10-12 22:19:08 +0200

    add route for users show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   get '/users' => 'users#index'
+  get '/users/show/:id' => "users#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
