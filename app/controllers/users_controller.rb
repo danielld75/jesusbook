773e90ef3281775576f4d826568bd3b107894d79
danielld75 <danielld@poczta.fm>
2015-10-12 21:45:10 +0200

    remove create from users controller and add show and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def create
-    @user = User.create(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'index'
-    end
+  def show
+    @user = User.find(params[:id])
   end
 
   def update
