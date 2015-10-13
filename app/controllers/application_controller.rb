c27fe1a692d6ae015310e025207115086b367fd9
danielld75 <danielld@poczta.fm>
2015-10-13 17:22:33 +0200

    remove redirect after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  sign_in_and_redirect @user
 end
