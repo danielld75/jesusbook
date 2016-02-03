c38850ce3fe419a2379882febb7198635f1c6746
danielld75 <danielld@poczta.fm>
2016-02-03 14:55:45 +0100

    turn off confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :baptism, dependent: :destroy
   has_one :confirmation, dependent: :destroy
   has_one :marriage, dependent: :destroy
