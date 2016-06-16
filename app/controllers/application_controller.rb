3e03dd5ff80be403ee5598473836c8aa27322fa8
danielld75 <danielld@poczta.fm>
2016-06-16 10:53:46 +0200

    add avatar for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :aboutMe, :name, :lastName, :birthday, :religion) end
-    devise_parameter_sanitizer.for(:account_update) do |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :aboutMe, :name, :lastName, :birthday, :religion) end
+    devise_parameter_sanitizer.for(:sign_up) do |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :aboutMe, :name, :lastName, :birthday, :religion, :avatar) end
+    devise_parameter_sanitizer.for(:account_update) do |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :aboutMe, :name, :lastName, :birthday, :religion, :avatar) end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ActiveRecord::Base
   has_one :priesthood, dependent: :destroy
   has_many :posts, dependent: :destroy
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
