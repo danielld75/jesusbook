dce30fe64af7e95c74997d19368fd2e0d2bb2417
danielld75 <danielld@poczta.fm>
2016-06-29 10:41:54 +0200

    cobfig assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Jesusbook
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pl
