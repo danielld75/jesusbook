4d6c1fdd0283a6915b2573982668dd64113fed5c
danielld75 <danielld@poczta.fm>
2016-06-30 10:26:54 +0200

    default config for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
-    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-    :url => "/system/:attachment/:id/:style/:filename",
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
