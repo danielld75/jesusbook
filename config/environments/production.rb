2430ca86e322d69d22a3cd8905a7c5e255bf35f1
danielld75 <danielld@poczta.fm>
2016-06-30 09:32:55 +0200

    static false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable serving static files from the `/public` folder by default since
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
