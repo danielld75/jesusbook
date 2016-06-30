e2fda80585b0a5054d19b59c14807d1ecd5f4a0f
danielld75 <danielld@poczta.fm>
2016-06-30 10:18:06 +0200

    configure url and path for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
-    url: "jesusbook/current/public/assets/images/:class/:attachment/:id_partition/:style/:filename",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename",
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
