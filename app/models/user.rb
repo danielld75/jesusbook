cd09b15e037cc199c6f6876631c1dd3c693be444
danielld75 <danielld@poczta.fm>
2016-06-30 10:05:09 +0200

    add url for paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ActiveRecord::Base
   has_one :priesthood, dependent: :destroy
   has_many :posts, dependent: :destroy
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+    url: ":rails_root/public/assets/:class/:attachment/:id_partition/:style/:filename",
+    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
