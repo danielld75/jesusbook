f8997425d6adb712d1b4f4750638dd95afdae41d
danielld75 <danielld@poczta.fm>
2018-01-27 10:37:57 +0100

    nokogiri gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,15 +28,15 @@ set :shared_paths, ['config/database.yml', 'log', 'config/secrets.yml']
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
-# task :environment do
-#   command %{
-#     echo "-----> Loading environment"
-#     #{echo_cmd %[source ~/.bashrc]}
-#           }
-#   invoke :'rbenv:load'
-#   # If you're using rbenv, use this to load the rbenv environment.
-#   # Be sure to commit your .rbenv-version to your repository.
-# end
+task :environment do
+  command %{
+    echo "-----> Loading environment"
+    #{echo_cmd %[source ~/.bashrc]}
+          }
+  invoke :'rbenv:load'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+end
 
 task :'rbenv:load' do
   comment %{Loading rbenv}
